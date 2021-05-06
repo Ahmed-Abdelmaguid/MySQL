4d530198def830565c80d3b13a8de9dc555fdb4a
Ahmed-Abdelmaguid <hitch96@my.yorku.ca>
2021-05-06 18:18:04 -0400

    create table likes

--- ig_clone.sql
+++ ig_clone.sql
@@ -16,7 +16,26 @@ CREATE TABLE photos (
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
+CREATE TABLE comments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    comment_text VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    photo_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id)
+);
+
+CREATE TABLE likes (
+    user_id INTEGER NOT NULL,
+    photo_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    PRIMARY KEY(user_id, photo_id)
+);
+
 INSERT INTO users (username) VALUES 
 ('Monkey'),
-('Lion');
+('Lion'),
 ('Tiger');
